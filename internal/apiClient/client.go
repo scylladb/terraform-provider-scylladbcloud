64620ad2c654bbb3931a0425edafb273827f0eea
Jakub Molinski <jakub@molinski.dev>
2022-07-09 21:13:19 +0200

    Move data object definitions to separate file, add timeout to http client

--- internal/apiClient/client.go
+++ internal/apiClient/client.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"sync"
 	"time"
 )
 
@@ -28,62 +27,31 @@ type Client struct {
 	// API endpoint
 	endpoint string
 
-	// Client is the underlying HTTP client used to run the requests.
-	Client *http.Client
-
-	// Ensures that the timeDelta function is only ran once
-	// sync.Once would consider init done, even in case of error
-	// hence a good old flag
-	timeDeltaMutex *sync.Mutex
-	timeDeltaDone  bool
-	timeDelta      time.Duration
-	Timeout        time.Duration
+	// httpClient is the underlying HTTP client used to run the requests.
+	httpClient *http.Client
 }
 
 // NewClient represents a new client to call the API
 func NewClient(endpoint, token string) (*Client, error) {
 	client := Client{
-		token:          token,
-		Client:         &http.Client{},
-		timeDeltaMutex: &sync.Mutex{},
-		timeDeltaDone:  false,
-		Timeout:        time.Duration(DefaultTimeout),
-		endpoint:       endpoint,
+		token: token,
+		httpClient: &http.Client{
+			Timeout: DefaultTimeout,
+		},
+		endpoint: endpoint,
 	}
 
-	if err := client.findAccountId(); err != nil {
+	if err := client.findAndSaveAccountId(); err != nil {
 		return nil, err
 	}
 
 	return &client, nil
 }
 
-type UserAccount struct {
-	UserId            int64  `json:"UserID"`
-	AccountId         int64  `json:"AccountID"`
-	Name              string `json:"Name"`
-	OwnerUserId       int64  `json:"OwnerUserID"`
-	AccountStatus     string `json:"AccountStatus"`
-	Role              string `json:"Role"`
-	UserAccountStatus string `json:"UserAccountStatus"`
-}
-
-func (c *Client) findAccountId() error {
-	var result UserAccount
-	if err := c.Get("/account/default", &result); err != nil {
-		return err
-	}
-
-	c.accountId = result.AccountId
-	return nil
-}
-
-// Don't review it, it'll be overhauled later.
-func (c *Client) Get(path string, resultType interface{}) error {
+func (c *Client) get(path string, resultType interface{}) error {
 	url := c.endpoint + path
 
-	httpClient := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 
 	if err != nil {
 		return err
@@ -92,7 +60,7 @@ func (c *Client) Get(path string, resultType interface{}) error {
 	req.Header.Add("accept", "application/json")
 	req.Header.Add("Authorization", "Bearer "+c.token)
 
-	res, err := httpClient.Do(req)
+	res, err := c.httpClient.Do(req)
 	if err != nil {
 		return err
 	}
@@ -120,90 +88,31 @@ func (c *Client) Get(path string, resultType interface{}) error {
 	return nil
 }
 
-type CloudProvider struct {
-	Id            int64  `json:"ID"`
-	Name          string `json:"Name"`
-	RootAccountId string `json:"RootAccountID"`
+func (c *Client) findAndSaveAccountId() error {
+	var result UserAccount
+	if err := c.get("/account/default", &result); err != nil {
+		return err
+	}
+
+	c.accountId = result.AccountId
+	return nil
 }
 
 func (c *Client) ListCloudProviders() ([]CloudProvider, error) {
 	var result []CloudProvider
-	if err := c.Get("/deployment/provider", &result); err != nil {
+	if err := c.get("/deployment/provider", &result); err != nil {
 		return nil, err
 	}
 	return result, nil
-}
-
-type CloudProviderRegion struct {
-	Id                          int64  `json:"ID"`
-	CloudProviderId             int64  `json:"CloudProviderID"`
-	Name                        string `json:"Name"`
-	FullName                    string `json:"FullName"`
-	ExternalId                  string `json:"ExternalID"`
-	MultiRegionExternalId       string `json:"MultiRegionExternalID"`
-	DcName                      string `json:"DCName"`
-	BackupStorageGbCost         string `json:"BackupStorageGBCost"`
-	TrafficSameRegionInGbCost   string `json:"TrafficSameRegionInGBCost"`
-	TrafficSameRegionOutGbCost  string `json:"TrafficSameRegionOutGBCost"`
-	TrafficCrossRegionOutGbCost string `json:"TrafficCrossRegionOutGBCost"`
-	TrafficInternetOutGbCost    string `json:"TrafficInternetOutGBCost"`
-	Continent                   string `json:"Continent"`
 }
 
 func (c *Client) ListCloudProviderRegions(providerId int64) ([]CloudProviderRegion, error) {
 	var result []CloudProviderRegion
 	path := fmt.Sprintf("/deployment/provider/%d/region", providerId)
-	if err := c.Get(path, &result); err != nil {
+	if err := c.get(path, &result); err != nil {
 		return nil, err
 	}
 	return result, nil
-}
-
-type DataCenter struct {
-	Id                               int64    `json:"ID"`
-	ClusterId                        int64    `json:"ClusterID"`
-	CloudProviderId                  int64    `json:"CloudProviderID"`
-	CloudProviderRegionId            int64    `json:"CloudProviderRegionID"`
-	ReplicationFactor                int64    `json:"ReplicationFactor"`
-	Ipv4Cidr                         string   `json:"IPv4CIDR"`
-	AccountCloudProviderCredentialId int64    `json:"AccountCloudProviderCredentialID"`
-	Status                           string   `json:"Status"`
-	Name                             string   `json:"Name"`
-	ManagementNetwork                string   `json:"ManagementNetwork"`
-	InstanceTypeId                   int64    `json:"InstanceTypeID"`
-	ClientConnection                 []string `json:"ClientConnection"`
-}
-
-type FreeTier struct {
-	ExpirationDate    string `json:"ExpirationDate"`
-	ExpirationSeconds int64  `json:"ExpirationSeconds"`
-	CreationTime      string `json:"CreationTime"`
-}
-
-type Cluster struct {
-	Id                        int64        `json:"ID"`
-	Name                      string       `json:"Name"`
-	ClusterNameOnConfigFile   string       `json:"ClusterNameOnConfigFile"`
-	Status                    string       `json:"Status"`
-	CloudProviderId           int64        `json:"CloudProviderID"`
-	ReplicationFactor         int64        `json:"ReplicationFactor"`
-	BroadcastType             string       `json:"BroadcastType"`
-	ScyllaVersionId           int64        `json:"ScyllaVersionID"`
-	ScyllaVersion             string       `json:"ScyllaVersion"`
-	Dc                        []DataCenter `json:"DC"`
-	GrafanaUrl                string       `json:"GrafanaURL"`
-	GrafanaRootUrl            string       `json:"GrafanaRootURL"`
-	BackofficeGrafanaUrl      string       `json:"BackofficeGrafanaURL"`
-	BackofficePrometheusUrl   string       `json:"BackofficePrometheusURL"`
-	BackofficeAlertManagerUrl string       `json:"BackofficeAlertManagerURL"`
-	FreeTier                  FreeTier     `json:"FreeTier"`
-	EncryptionMode            string       `json:"EncryptionMode"`
-	UserApiInterface          string       `json:"UserAPIInterface"`
-	PricingModel              int64        `json:"PricingModel"`
-	MaxAllowedCidrRange       int64        `json:"MaxAllowedCidrRange"`
-	CreatedAt                 string       `json:"CreatedAt"`
-	Dns                       bool         `json:"DNS"`
-	PromProxyEnabled          bool         `json:"PromProxyEnabled"`
 }
 
 func (c *Client) ListClusters() ([]Cluster, error) {
@@ -213,7 +122,7 @@ func (c *Client) ListClusters() ([]Cluster, error) {
 	}
 	var result []Item
 	path := fmt.Sprintf("/account/%d/cluster", c.accountId)
-	if err := c.Get(path, &result); err != nil {
+	if err := c.get(path, &result); err != nil {
 		return nil, err
 	}
 
---
+++ internal/apiClient/models.go
@@ -0,0 +1,80 @@
+package apiClient
+
+type UserAccount struct {
+	UserId            int64  `json:"UserID"`
+	AccountId         int64  `json:"AccountID"`
+	Name              string `json:"Name"`
+	OwnerUserId       int64  `json:"OwnerUserID"`
+	AccountStatus     string `json:"AccountStatus"`
+	Role              string `json:"Role"`
+	UserAccountStatus string `json:"UserAccountStatus"`
+}
+
+type CloudProvider struct {
+	Id            int64  `json:"ID"`
+	Name          string `json:"Name"`
+	RootAccountId string `json:"RootAccountID"`
+}
+
+type CloudProviderRegion struct {
+	Id                          int64  `json:"ID"`
+	CloudProviderId             int64  `json:"CloudProviderID"`
+	Name                        string `json:"Name"`
+	FullName                    string `json:"FullName"`
+	ExternalId                  string `json:"ExternalID"`
+	MultiRegionExternalId       string `json:"MultiRegionExternalID"`
+	DcName                      string `json:"DCName"`
+	BackupStorageGbCost         string `json:"BackupStorageGBCost"`
+	TrafficSameRegionInGbCost   string `json:"TrafficSameRegionInGBCost"`
+	TrafficSameRegionOutGbCost  string `json:"TrafficSameRegionOutGBCost"`
+	TrafficCrossRegionOutGbCost string `json:"TrafficCrossRegionOutGBCost"`
+	TrafficInternetOutGbCost    string `json:"TrafficInternetOutGBCost"`
+	Continent                   string `json:"Continent"`
+}
+
+type DataCenter struct {
+	Id                               int64    `json:"ID"`
+	ClusterId                        int64    `json:"ClusterID"`
+	CloudProviderId                  int64    `json:"CloudProviderID"`
+	CloudProviderRegionId            int64    `json:"CloudProviderRegionID"`
+	ReplicationFactor                int64    `json:"ReplicationFactor"`
+	Ipv4Cidr                         string   `json:"IPv4CIDR"`
+	AccountCloudProviderCredentialId int64    `json:"AccountCloudProviderCredentialID"`
+	Status                           string   `json:"Status"`
+	Name                             string   `json:"Name"`
+	ManagementNetwork                string   `json:"ManagementNetwork"`
+	InstanceTypeId                   int64    `json:"InstanceTypeID"`
+	ClientConnection                 []string `json:"ClientConnection"`
+}
+
+type FreeTier struct {
+	ExpirationDate    string `json:"ExpirationDate"`
+	ExpirationSeconds int64  `json:"ExpirationSeconds"`
+	CreationTime      string `json:"CreationTime"`
+}
+
+type Cluster struct {
+	Id                        int64        `json:"ID"`
+	Name                      string       `json:"Name"`
+	ClusterNameOnConfigFile   string       `json:"ClusterNameOnConfigFile"`
+	Status                    string       `json:"Status"`
+	CloudProviderId           int64        `json:"CloudProviderID"`
+	ReplicationFactor         int64        `json:"ReplicationFactor"`
+	BroadcastType             string       `json:"BroadcastType"`
+	ScyllaVersionId           int64        `json:"ScyllaVersionID"`
+	ScyllaVersion             string       `json:"ScyllaVersion"`
+	Dc                        []DataCenter `json:"DC"`
+	GrafanaUrl                string       `json:"GrafanaURL"`
+	GrafanaRootUrl            string       `json:"GrafanaRootURL"`
+	BackofficeGrafanaUrl      string       `json:"BackofficeGrafanaURL"`
+	BackofficePrometheusUrl   string       `json:"BackofficePrometheusURL"`
+	BackofficeAlertManagerUrl string       `json:"BackofficeAlertManagerURL"`
+	FreeTier                  FreeTier     `json:"FreeTier"`
+	EncryptionMode            string       `json:"EncryptionMode"`
+	UserApiInterface          string       `json:"UserAPIInterface"`
+	PricingModel              int64        `json:"PricingModel"`
+	MaxAllowedCidrRange       int64        `json:"MaxAllowedCidrRange"`
+	CreatedAt                 string       `json:"CreatedAt"`
+	Dns                       bool         `json:"DNS"`
+	PromProxyEnabled          bool         `json:"PromProxyEnabled"`
+}
