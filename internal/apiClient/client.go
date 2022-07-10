730aa42dc7126e1aa168e653ff9bbc3c6a9f2cf6
Jakub Molinski <jakub@molinski.dev>
2022-07-10 14:30:04 +0200

    Move public API methods to a separate file

--- internal/apiClient/client.go
+++ internal/apiClient/client.go
@@ -6,7 +6,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -165,42 +164,4 @@ func (c *Client) findAndSaveAccountId() error {
 
 	c.accountId = result.AccountId
 	return nil
-}
-
-func (c *Client) ListCloudProviders() ([]CloudProvider, error) {
-	var result []CloudProvider
-	if err := c.get("/deployment/provider", &result); err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-func (c *Client) ListCloudProviderRegions(providerId int64) ([]CloudProviderRegion, error) {
-	var result []CloudProviderRegion
-	path := fmt.Sprintf("/deployment/provider/%d/region", providerId)
-	if err := c.get(path, &result); err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-func (c *Client) ListClusters() ([]Cluster, error) {
-	type Item struct {
-		Value Cluster     `json:"Value"`
-		Error interface{} `json:"Error"`
-	}
-	var result []Item
-	path := fmt.Sprintf("/account/%d/cluster", c.accountId)
-	if err := c.get(path, &result); err != nil {
-		return nil, err
-	}
-
-	clusters := make([]Cluster, len(result))
-	for i, item := range result {
-		if item.Error != nil {
-			return nil, errors.New(fmt.Sprintf("cluster error: %v", item.Error))
-		}
-		clusters[i] = item.Value
-	}
-	return clusters, nil
 }
---
+++ internal/apiClient/endpoints.go
@@ -0,0 +1,44 @@
+package apiClient
+
+import (
+	"errors"
+	"fmt"
+)
+
+func (c *Client) ListCloudProviders() ([]CloudProvider, error) {
+	var result []CloudProvider
+	if err := c.get("/deployment/provider", &result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (c *Client) ListCloudProviderRegions(providerId int64) ([]CloudProviderRegion, error) {
+	var result []CloudProviderRegion
+	path := fmt.Sprintf("/deployment/provider/%d/region", providerId)
+	if err := c.get(path, &result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (c *Client) ListClusters() ([]Cluster, error) {
+	type Item struct {
+		Value Cluster     `json:"Value"`
+		Error interface{} `json:"Error"`
+	}
+	var result []Item
+	path := fmt.Sprintf("/account/%d/cluster", c.accountId)
+	if err := c.get(path, &result); err != nil {
+		return nil, err
+	}
+
+	clusters := make([]Cluster, len(result))
+	for i, item := range result {
+		if item.Error != nil {
+			return nil, errors.New(fmt.Sprintf("cluster error: %v", item.Error))
+		}
+		clusters[i] = item.Value
+	}
+	return clusters, nil
+}
