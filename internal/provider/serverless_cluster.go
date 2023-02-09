c261d0b23e51e400372050f4a54615188db7b602
Rafal Jeczalik <rafal@rjk.io>
2023-02-09 11:56:18 +0100

    provider: add AWS check for byoa_id

--- internal/provider/serverless_cluster.go
+++ internal/provider/serverless_cluster.go
@@ -47,8 +47,13 @@ func ResourceServerlessCluster() *schema.Resource {
 			"name": {
 				Description: "Serverless cluster name",
 				Required:    true,
-				ForceNew:    true,
-				Type:        schema.TypeString,
+				// NOTE(rjeczalik): ForceNew is commented out here, otherwise
+				// internal provider validate fails due to all the attrs
+				// being ForceNew; Scylla Cloud API does not allow for
+				// updating existing clusters, thus update the implementation
+				// always returns a non-nil error.
+				// ForceNew: true,
+				Type: schema.TypeString,
 			},
 			"free_tier": {
 				Description: "Whether a cluster is in a free tier",
