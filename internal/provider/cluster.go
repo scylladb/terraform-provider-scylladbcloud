8f2c521909eb57d3584f6859642ce72e2e5e32b1
Dmitry Kropachev <dmitry.kropachev@scylladb.com>
2023-08-23 09:04:32 -0400

    fix(provider): add commet to cluster

--- internal/provider/cluster.go
+++ internal/provider/cluster.go
@@ -135,8 +135,6 @@ func ResourceCluster() *schema.Resource {
 				Description: "Whether to enable CNAME for seed nodes",
 				Optional:    true,
 				Type:        schema.TypeBool,
-				// NOTE(rjeczalik): ForceNew is commented out here, otherwise
-				// internal provider validate fails due to all the attrs
 				// being ForceNew; Scylla Cloud API does not allow for
 				// updating existing clusters, thus update the implementation
 				// always returns a non-nil error.
