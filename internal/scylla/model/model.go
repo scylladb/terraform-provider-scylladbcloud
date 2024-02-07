e025e7e4bd1a7bab2da13903651312fc1153739c
charconstpointer <attwistingnether@gmail.com>
2024-02-07 10:15:14 +0100

    chore: rename allowCql json tag

--- internal/scylla/model/model.go
+++ internal/scylla/model/model.go
@@ -241,7 +241,7 @@ type VPCPeering struct {
 	ProjectID        string   `json:"projectID"`
 	Status           string   `json:"status"`
 	ExpiresAt        string   `json:"expiresAt"`
-	AllowCQL         string   `json:"allowCQL"`
+	AllowCQL         string   `json:"allowCql"`
 }
 
 func (vp *VPCPeering) NetworkLink() string {
