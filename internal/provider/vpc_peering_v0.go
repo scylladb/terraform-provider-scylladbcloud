b35ed0237f8b3dd69ca2fd5bc1e8924b39a21bd5
rjeczalik <rafal@rjk.io>
2024-02-08 14:41:06 +0100

    resource/vpc_peering: deprecate peer_cidr_block attribute (schema version 1 + migration) (#113)
    This PR deprecates the peer_cidr_block attribute, in favor of more generic peer_cidr_blocks one.
    
    Any existing terraform resources which use the `peer_cidr_block` attribute need to be updated
    to use `peer_cidr_blocks` one, which is usually as simple as changing the following:
    
        resource "scylladbcloud_vpc_peering" "x" {
            ...
    
            peer_cidr_block = "10.0.0.0/16"
    
            ...
        }
    
    to:
    
        resource "scylladbcloud_vpc_peering" "x" {
            ...
    
            peer_cidr_blocks = ["10.0.0.0/16"]
    
            ...
        }
    
    Also this resource schema will be version at 1 and any old state file will be migrated automatically.

---
+++ internal/provider/vpc_peering_v0.go
@@ -0,0 +1,66 @@
+package provider
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceVPCPeeringV0() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"cluster_id": {
+				Required: true,
+				Type:     schema.TypeInt,
+			},
+			"datacenter": {
+				Required: true,
+				ForceNew: true,
+				Type:     schema.TypeString,
+			},
+			"peer_vpc_id": {
+				Required: true,
+				ForceNew: true,
+				Type:     schema.TypeString,
+			},
+			"peer_cidr_block": {
+				Optional: true,
+				ForceNew: true,
+				Type:     schema.TypeString,
+			},
+			"peer_cidr_blocks": {
+				Optional: true,
+				ForceNew: true,
+				Type:     schema.TypeList,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"peer_region": {
+				Required: true,
+				ForceNew: true,
+				Type:     schema.TypeString,
+			},
+			"peer_account_id": {
+				Required: true,
+				ForceNew: true,
+				Type:     schema.TypeString,
+			},
+			"allow_cql": {
+				Optional: true,
+				Type:     schema.TypeBool,
+				Default:  true,
+			},
+			"vpc_peering_id": {
+				Computed: true,
+				Type:     schema.TypeInt,
+			},
+			"connection_id": {
+				Computed: true,
+				Type:     schema.TypeString,
+			},
+			"network_link": {
+				Computed: true,
+				Type:     schema.TypeString,
+			},
+		},
+	}
+}
